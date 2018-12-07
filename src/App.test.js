c9a68ea2fe24821ba364f9980f7ac2555f34ead7
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-07 01:21:00 +0000

    adding test for totals

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
-import {add} from './App.js';
+import {add, total} from './App.js';
 
 test('add', () => {
     const value = add(1, 2);
     expect(value).toBe(3);
+});
+
+test('total', () => {
+    expect(total(5, 20)).toBe('£25');
 });
