7424b803e5d31063604be9609ca48a0bee7e3910
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-07 03:00:48 +0000

    adding mock function

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
-import {add, total} from './App.js';
+import {total} from './App.js';
+
+//mock function that gives you results without access to the 
+//actual function
+const add = jest.fn(() => 3);
 
 test('add', () => {
-    const value = add(1, 2);
-    expect(value).toBe(3);
+    //const value = add(1, 2);
+    expect(add(1, 2)).toBe(3);
+    expect(add).toHaveBeenCalledTimes(1);
+    expect(add).toHaveBeenCalledWith(1, 2);
 });
 
-test('total', () => {
-    expect(total(5, 20)).toBe('£25');
-});
+// test('total', () => {
+//     expect(total(5, 20)).toBe('£25');
+// });
